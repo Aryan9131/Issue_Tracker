bd21664151a28a3d5aa1577ef30e209b68aca73a
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2023-12-25 12:39:23 +0530

    Update index.js
    Added MongoDB atlas connection string

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(session({
         maxAge: (1000 * 60 * 100) // Session timeout duration
     },
     store: MongoStore.create({
-        mongoUrl: `mongodb://127.0.0.1:27017/${env.db}`,
+        mongoUrl: `mongodb+srv://aryannayak9131:AhGzJJtziaZKPCYo@cluster0.luuffqt.mongodb.net/${env.db}?retryWrites=true&w=majority`,
         autoRemove: 'disable'
     })
 }));
