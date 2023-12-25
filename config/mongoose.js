ea331cd08443e45c9789ca9e8882f5d9fd9ab039
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2023-12-25 12:41:18 +0530

    Update mongoose.js
    Added MongoDB atlas connection string

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const env=require('./environment');
 let db;
 async function main(){
-    db=await mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`)
+    db=await mongoose.connect(`mongodb+srv://aryannayak9131:AhGzJJtziaZKPCYo@cluster0.luuffqt.mongodb.net/${env.db}?retryWrites=true&w=majority`)
 } 
  
 main().then(()=>{
